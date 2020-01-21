8162b253a3378fbc82d53fe45c03cd8b7edddda5
ilaydademoglu98 <60137415+ilaydademoglu98@users.noreply.github.com>
2020-01-21 15:06:11 +0300

    Delete t6.py

--- HW4/t6.py
+++
@@ -1,8 +0,0 @@
-if True:
-     while True:
-          for x in range(1,20):
-                  if True:
-                      while True:
-                          else
-                                   l =
-print "End"
